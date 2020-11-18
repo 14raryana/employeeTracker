918d556b02de46ba7b068fa62599d38c8ca4dd8e
Raman Aryana <14raryana@gmail.com>
2020-11-18 16:24:25 -0500

    fixed the schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,28 @@
 CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE departments(
+	id INT auto_increment NOT NULL,
+    departmentName VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles(
+	id INT auto_increment NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees(
+	id INT auto_increment NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    PRIMARY KEY (id)
+);
